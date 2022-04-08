e2bb31057ba6e2e415205b74d03420a703a2bf5b
Florian Cassayre <florian.cassayre@gmail.com>
2022-04-08 09:32:42 +0200

    Update macros to support unapply

--- src/main/scala/me/cassayre/florian/masterproject/front/parser/FrontMacro.scala
+++ src/main/scala/me/cassayre/florian/masterproject/front/parser/FrontMacro.scala
@@ -11,23 +11,66 @@ object FrontMacro {
 
   import scala.quoted.*
 
+  // https://github.com/lampepfl/dotty/issues/8577#issuecomment-1014729373
+
   extension (inline sc: StringContext)
-    inline def term(inline args: Any*): Term = ${ termImpl('sc, 'args) }
-    inline def formula(inline args: Any*): Formula = ${ formulaImpl('sc, 'args) }
-    inline def sequent(inline args: Any*): Sequent = ${ sequentImpl('sc, 'args) }
-    inline def partial(inline args: Any*): PartialSequent = ${ partialImpl('sc, 'args) }
+    transparent inline def term: Any = ${ SIParts.scMacro[TermParts]('sc) }
+    transparent inline def formula: Any = ${ SIParts.scMacro[FormulaParts]('sc) }
+    transparent inline def sequent: Any = ${ SIParts.scMacro[SequentParts]('sc) }
+    transparent inline def partial: Any = ${ SIParts.scMacro[PartialSequentParts]('sc) }
 
-  private def termImpl(sc: Expr[StringContext], args: Expr[Seq[Any]])(using Quotes): Expr[Term] =
-    '{ FrontReader.readTerm(${rawStringImpl(sc, args)}) }
-  private def formulaImpl(sc: Expr[StringContext], args: Expr[Seq[Any]])(using Quotes): Expr[Formula] =
-    '{ FrontReader.readFormula(${rawStringImpl(sc, args)}) }
-  private def sequentImpl(sc: Expr[StringContext], args: Expr[Seq[Any]])(using Quotes): Expr[Sequent] =
-    '{ FrontReader.readSequent(${rawStringImpl(sc, args)}) }
-  private def partialImpl(sc: Expr[StringContext], args: Expr[Seq[Any]])(using Quotes): Expr[PartialSequent] =
-    '{ FrontReader.readPartialSequent(${rawStringImpl(sc, args)}) }
+  class TermParts[P <: Tuple](parts: P) {
+    transparent inline def apply(inline args: Any*): Term = ${ termApplyMacro('parts, 'args) }
+    //transparent inline def unapplySeq(inline arg: Any): Option[Seq[Any]] = ${ termUnapplyMacro('parts, 'arg) }
+  }
+  class FormulaParts[P <: Tuple](parts: P) {
+    transparent inline def apply(inline args: Any*): Formula = ${ formulaApplyMacro('parts, 'args) }
+  }
+  class SequentParts[P <: Tuple](parts: P) {
+    transparent inline def apply(inline args: Any*): Sequent = ${ sequentApplyMacro('parts, 'args) }
+  }
+  class PartialSequentParts[P <: Tuple](parts: P) {
+    transparent inline def apply(inline args: Any*): PartialSequent = ${ partialSequentApplyMacro('parts, 'args) }
+  }
+
+  trait SIParts[P <: Tuple](parts: P)
+  object SIParts {
+    def scMacro[SI[_ <: Tuple]](sc: Expr[StringContext])(using Quotes, Type[SI]): Expr[Any] = {
+      import quotes.reflect.*
+      val '{StringContext(${Varargs(args)}*)} = sc
+      val tplExpr = Expr.ofTupleFromSeq(args)
+      val tplTpe = tplExpr.asTerm.tpe
+      val AppliedType(siTpe, _) = TypeRepr.of[SI[Tuple]]
+      val siSym = siTpe.typeSymbol
+      val siTree =
+        New(TypeTree.of[SI[Tuple]])
+          .select(siSym.primaryConstructor)
+          .appliedToType(tplTpe)
+          .appliedTo(tplExpr.asTerm)
+      siTree.asExpr
+    }
+  }
 
 
-  private def rawStringImpl(sc: Expr[StringContext], args: Expr[Seq[Any]])(using q: Quotes): Expr[String] = {
+  /*private def termUnapplyMacro[P <: Tuple](parts: Expr[P], arg: Expr[Any])(using Quotes, Type[P]): Expr[Option[Seq[Any]]] = {
+    '{ None: Option[Seq[Term]] }
+  }*/
+
+
+  private def termApplyMacro[P <: Tuple](parts: Expr[P], args: Expr[Seq[Any]])(using Quotes, Type[P]): Expr[Term] =
+    '{ FrontReader.readTerm(${rawStringMacro(parts, args)}) }
+  private def formulaApplyMacro[P <: Tuple](parts: Expr[P], args: Expr[Seq[Any]])(using Quotes, Type[P]): Expr[Formula] =
+    '{ FrontReader.readFormula(${rawStringMacro(parts, args)}) }
+  private def sequentApplyMacro[P <: Tuple](parts: Expr[P], args: Expr[Seq[Any]])(using Quotes, Type[P]): Expr[Sequent] =
+    '{ FrontReader.readSequent(${rawStringMacro(parts, args)}) }
+  private def partialSequentApplyMacro[P <: Tuple](parts: Expr[P], args: Expr[Seq[Any]])(using Quotes, Type[P]): Expr[PartialSequent] =
+    '{ FrontReader.readPartialSequent(${rawStringMacro(parts, args)}) }
+
+  private def partsAsRawStringsMacro[P <: Tuple](parts: Expr[P])(using Quotes, Type[P]): Expr[Seq[String]] = {
+    '{ ${parts}.productIterator.toSeq.map(_.asInstanceOf[String]) }
+  }
+
+  private def rawStringMacro[P <: Tuple](parts: Expr[P], args: Expr[Seq[Any]])(using q: Quotes, tpe: Type[P]): Expr[String] = {
     import q.reflect._
 
     val argsSeq: Seq[Expr[Any]] = args match {
@@ -40,20 +83,10 @@ object FrontMacro {
       case '{ $f: ConstantFunctionLabel[?] | ConstantPredicateLabel[?] | ConstantConnectorLabel[?] } => '{ s"${${f}.id}" }
       case '{ $other } => '{ ${other}.toString }
     }
-    val partsSeq: Seq[Expr[String]] = sc match {
-      case '{ StringContext($vs: _*) } =>
-        vs match { case Varargs(es) => es }
-    }
-    val rawParts: Seq[String] = partsSeq.map(_.asTerm).map {
-      case Literal(c: Constant) => c.value.toString
-    }
-    val argsSeqWithEmpty: Seq[Expr[String]] = argsSeqString :+ '{""}
-    val allExprs: Seq[Expr[Any]] = rawParts.zip(argsSeqWithEmpty).flatMap { case (p: String, a: Expr[Any]) =>
-      Seq(Expr(p), a)
-    }
-    val exprString: Expr[String] = allExprs.map(e => '{ ${e}.toString }).reduce {
-      case (l: Expr[Any], r: Expr[Any]) => '{ $l + $r }
-    }
+    val partsSeq: Expr[Seq[String]] = '{ ${parts}.productIterator.toSeq.map(_.asInstanceOf[String]) }
+    val argsSeqWithEmpty: Expr[Seq[String]] = argsSeqString.foldRight('{Seq("")})((e, acc) => '{ $e +: $acc })
+    val exprString: Expr[String] = '{ ${partsSeq}.zip(${argsSeqWithEmpty}).flatMap { case (p, a) => Seq(p, a) }.reduce { case (l, r) => l + r } }
+
     exprString
   }
 
